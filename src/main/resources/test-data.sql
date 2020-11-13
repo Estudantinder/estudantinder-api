a74b2750639ebced0e7027541f135da309c264e6
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-11-13 02:32:12 -0300

    Atualiza os dados de teste para usar o hibernate_sequence

--- src/main/resources/test-data.sql
+++ src/main/resources/test-data.sql
@@ -1,4 +1,4 @@
-INSERT INTO school (id, name, address) VALUES (-1, 'SCHOOL 1', 'ADDRESS 1');
-INSERT INTO school (id, name, address) VALUES (-2, 'SCHOOL 2', 'ADDRESS 2');
-INSERT INTO school (id, name, address) VALUES (-3, 'SCHOOL 3', 'ADDRESS 3');
-INSERT INTO school (id, name, address) VALUES (-4, 'SCHOOL 4', 'ADDRESS 4');
+INSERT INTO school (id, name, address) VALUES (nextval('hibernate_sequence'), 'SCHOOL 1', 'ADDRESS 1');
+INSERT INTO school (id, name, address) VALUES (nextval('hibernate_sequence'), 'SCHOOL 2', 'ADDRESS 2');
+INSERT INTO school (id, name, address) VALUES (nextval('hibernate_sequence'), 'SCHOOL 3', 'ADDRESS 3');
+INSERT INTO school (id, name, address) VALUES (nextval('hibernate_sequence'), 'SCHOOL 4', 'ADDRESS 4');
