e2a62fe65b8897a8441da25fb9ac2a7d15c5750b
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-11-14 13:45:19 -0300

    Adiciona a validacao dos campos pelo DTO do estudante

--- src/main/java/org/estudantinder/features/Student/CreateStudent/Resource.java
+++ src/main/java/org/estudantinder/features/Student/CreateStudent/Resource.java
@@ -2,6 +2,7 @@ package org.estudantinder.features.Student.CreateStudent;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -28,7 +29,7 @@ public class Resource {
     @APIResponse(responseCode = "400", description = "No Data Sent")
     @APIResponse(responseCode = "409", description = "Email Already In Use")
     @APIResponse(responseCode = "500", description = "Unexpected Error")
-    public Response createStudent(StudentDTO data) throws Exception {
+    public Response createStudent(@Valid StudentDTO data) throws Exception {
         return createStudentController.handle(data);
     }
 }
