7742feb305227bcedf2feff84aa19597bff40136
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-11-25 09:51:04 -0300

    Atualiza a validacao de preferencias para permitir a omissao do ano

--- src/main/java/org/estudantinder/features/Student/CreateStudent/DTO/PreferencesDTO.java
+++ src/main/java/org/estudantinder/features/Student/CreateStudent/DTO/PreferencesDTO.java
@@ -5,8 +5,10 @@ import javax.validation.constraints.Min;
 
 public class PreferencesDTO {
     
-    @Min(message = "You must be in a school year between 1 - 3", value = 1)
+    @Min(message = "You must be in a school year between 1 - 3", value = 0)
     @Max(message = "You must be in a school year between 1 - 3", value = 3)
+    // int is not nullable
+    // 0 is used in that case
     public int schoolYear;
     public String schoolShift;
     public String gender;
