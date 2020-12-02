31f632552a9827e0e122e1f0358b5c4989e8692f
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-12-02 14:58:20 -0300

    Adiciona o teste da rota mostrar os likes do estudante

---
+++ src/test/java/org/estudantinder/Features/Users/ShowLikesTest.java
@@ -0,0 +1,33 @@
+package org.estudantinder.Features.Users;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.jwt.build.Jwt;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@QuarkusTest
+public class ShowLikesTest {
+
+    @Test
+    public void testmethodname() {
+        given()
+        .auth().oauth2(generateValidStudentToken())
+        .when().get("/users/likes")
+        .then()
+            .statusCode(200);
+    }
+
+    static String generateValidStudentToken() {
+        return Jwt.issuer("https://github.com/AdamAugustinsky")
+            .upn("estudantinder@quarkus.io")
+            .groups("Student")
+            .claim("id", 21)
+            .expiresAt(Instant.now().plus(2, ChronoUnit.MINUTES ))
+            .sign();
+    }
+}
