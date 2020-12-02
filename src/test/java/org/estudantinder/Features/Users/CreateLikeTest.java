1689a6af7221a6789460524e625cfb12248107c8
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-12-02 14:57:46 -0300

    Adiciona o teste da criação do like

---
+++ src/test/java/org/estudantinder/Features/Users/CreateLikeTest.java
@@ -0,0 +1,35 @@
+package org.estudantinder.Features.Users;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.jwt.build.Jwt;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@QuarkusTest
+public class CreateLikeTest {
+
+    @Test
+    public void testmethodname() {
+        given()
+        .pathParam("id", 20)
+        .auth().oauth2(generateValidStudentToken())
+        .when().post("/users/likes/{id}")
+        .then()
+            .statusCode(201);
+    }
+
+    static String generateValidStudentToken() {
+        return Jwt.issuer("https://github.com/AdamAugustinsky")
+            .upn("estudantinder@quarkus.io")
+            .groups("Student")
+            .claim("id", 21)
+            .expiresAt(Instant.now().plus(2, ChronoUnit.MINUTES ))
+            .sign();
+    }
+    
+}
