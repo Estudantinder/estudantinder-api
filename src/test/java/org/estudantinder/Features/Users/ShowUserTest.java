f2c1ad052b41afc3e307b680913b57cd17c81726
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-02-02 16:55:55 -0300

    test(showUser)

---
+++ src/test/java/org/estudantinder/Features/Users/ShowUserTest.java
@@ -0,0 +1,34 @@
+package org.estudantinder.Features.Users;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.jwt.build.Jwt;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+
+@QuarkusTest
+public class ShowUserTest {
+
+    @Test
+    public void testShowUserEndpoint() {
+        given()
+            .auth().oauth2(generateValidStudentToken())
+            .when().get("/users")
+            .then()
+                .statusCode(200);
+    }
+
+    static String generateValidStudentToken() {
+        return Jwt.issuer("https://github.com/AdamAugustinsky")
+            .upn("estudantinder@quarkus.io")
+            .groups("User")
+            .claim("id", 22)
+            .expiresAt(Instant.now().plus(2, ChronoUnit.MINUTES ))
+            .sign(); 
+    }
+}
