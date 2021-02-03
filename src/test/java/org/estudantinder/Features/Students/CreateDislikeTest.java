98cd12894172bc07d011c92466ca50ae39e7fb18
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-02-03 08:53:18 -0300

    test(createDislike)

---
+++ src/test/java/org/estudantinder/Features/Students/CreateDislikeTest.java
@@ -0,0 +1,35 @@
+package org.estudantinder.Features.Students;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.jwt.build.Jwt;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@QuarkusTest
+public class CreateDislikeTest {
+
+    @Test
+    public void testCreateDislikeEndpoint() {
+        given()
+        .pathParam("id", 24)
+        .auth().oauth2(generateValidStudentToken())
+        .when().post("/students/dislikes/{id}")
+        .then()
+            .statusCode(201);
+    }
+
+    static String generateValidStudentToken() {
+        return Jwt.issuer("https://github.com/AdamAugustinsky")
+            .upn("estudantinder@quarkus.io")
+            .groups("User")
+            .claim("id", 22)
+            .expiresAt(Instant.now().plus(2, ChronoUnit.MINUTES ))
+            .sign();
+    }
+    
+}
