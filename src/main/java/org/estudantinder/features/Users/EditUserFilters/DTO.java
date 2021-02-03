37f1954afb18f6de56efbe91069df489e4171a32
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-02-03 07:02:54 -0300

    feat(editUserFilters): adiciona subjects_ids

--- src/main/java/org/estudantinder/features/Users/EditUserFilters/DTO.java
+++ src/main/java/org/estudantinder/features/Users/EditUserFilters/DTO.java
@@ -1,7 +1,10 @@
 package org.estudantinder.features.Users.EditUserFilters;
 
+import java.util.List;
+
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 public class DTO {
     
@@ -11,11 +14,12 @@ public class DTO {
     // 0 is used in that case
     public int school_year;
     
-    @Min(message = "Shift must be between 1(morning) and 2(evening)", value = 0)
     @Max(message = "Shift must be between 1(morning) and 2(evening)", value = 2)
     public int shift;
     public String gender;
 
     public Long course_id;
 
+    @Size(message = "Your number of subjects must be between 1 and 3", max = 3)
+    public List<Long> subjects_ids;
 }
--- src/main/java/org/estudantinder/features/Users/EditUserFilters/Feature.java
+++ src/main/java/org/estudantinder/features/Users/EditUserFilters/Feature.java
@@ -1,15 +1,23 @@
 package org.estudantinder.features.Users.EditUserFilters;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityNotFoundException;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.NotFoundException;
 
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.estudantinder.entities.Course;
 import org.estudantinder.entities.Preferences;
+import org.estudantinder.entities.Subject;
 import org.estudantinder.entities.Users;
 import org.estudantinder.repositories.CoursesRepository;
+import org.estudantinder.repositories.SubjectsRepository;
 import org.estudantinder.repositories.UsersRepository;
 
 @ApplicationScoped
@@ -21,6 +29,9 @@ public class Feature {
     @Inject
     CoursesRepository coursesRepository;
 
+    @Inject
+    SubjectsRepository subjectsRepository;
+
     private Course returnCourseIfExists(Long courseId) {
         Course course = coursesRepository.findById(courseId);
 
@@ -31,30 +42,70 @@ public class Feature {
         return course;
     }
     
-    private void treatInvalidID(Users user) {
+    private void treatStudentInvalidID(Users user) {
         if(user == null) {
             throw new NotFoundException("Student ID not valid");
         }
     } 
 
-    private Preferences mapToPreferences(DTO preferences) {
-        Preferences newUserPreferences = new Preferences();
+    private void treatSubjectsDuplicatedID(List<Long> subjects_ids) {
+        if(isListFieldDuplicate(subjects_ids)) {
+            throw new BadRequestException("Subject ID's can't be duplicated");
+        }
+    }
+    public boolean isListFieldDuplicate(List<Long> subjects_id) {
+        final Set<Long> uniqueIds = new HashSet<>();
         
-        newUserPreferences.setGender(preferences.gender);
-        newUserPreferences.setShift(preferences.shift);
-        newUserPreferences.setSchool_year(preferences.school_year);
-        if (preferences.course_id != null) {
-            newUserPreferences.setCourse(returnCourseIfExists(preferences.course_id));
+        for (Long subject_id : subjects_id){
+            if (!uniqueIds.add(subject_id)){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    public Subject returnSubjectIfExists(Long subject_id) {
+        Subject subject = subjectsRepository.findById(subject_id);
+
+        if(subject == null) {
+            throw new EntityNotFoundException("Subject id "+subject_id+" Not Found");
         }
 
-        return newUserPreferences;
+        return subject;
+    }
+
+    public List<Subject> returnListOfSubjects(List<Long> subjects_ids) {
+        List<Subject> subjects = new ArrayList<Subject>();
+        
+        treatSubjectsDuplicatedID(subjects_ids);
+        subjects_ids.forEach(subject_id -> {
+            Subject subject = returnSubjectIfExists(subject_id);
+            subjects.add(subject);
+        });
+
+        return subjects;
+    } 
+
+    private Preferences mapToPreferences(DTO preferences) {
+        Preferences userPreferences = new Preferences();
+        
+        userPreferences.setGender(preferences.gender);
+        userPreferences.setShift(preferences.shift);
+        userPreferences.setSchool_year(preferences.school_year);
+        if (preferences.course_id != null) {
+            userPreferences.setCourse(returnCourseIfExists(preferences.course_id));
+        }
+        userPreferences.setSubjects(returnListOfSubjects(preferences.subjects_ids));
+
+        return userPreferences;
     }
 
     public Preferences execute(JsonWebToken jwt, DTO data) throws Exception {
         Long userId = Long.parseLong(jwt.getClaim("id").toString());
         Users authenticatedUser = usersRepository.findById(userId);
 
-        treatInvalidID(authenticatedUser);
+        treatStudentInvalidID(authenticatedUser);
 
         authenticatedUser.setPreferences(mapToPreferences(data));
 
