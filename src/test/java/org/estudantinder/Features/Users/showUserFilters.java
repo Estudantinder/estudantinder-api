67764722429bbcc4ed108bb333701de431dacb88
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-02-23 14:04:40 -0300

    test(showUserFilters)

---
+++ src/test/java/org/estudantinder/Features/Users/showUserFilters.java
@@ -0,0 +1,35 @@
+package org.estudantinder.Features.Users;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import io.smallrye.jwt.build.Jwt;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@QuarkusTest
+public class showUserFilters {
+
+    @Test
+    public void testEditUserFiltersEndpoint() {
+        given()
+        .auth().oauth2(generateValidStudentToken())
+        .contentType(ContentType.JSON)
+        .when().get("/users/filters")
+        .then()
+            .statusCode(200);
+    }
+
+    static String generateValidStudentToken() {
+        return Jwt.issuer("https://github.com/AdamAugustinsky")
+            .upn("estudantinder@quarkus.io")
+            .groups("User")
+            .claim("id", 22)
+            .expiresAt(Instant.now().plus(2, ChronoUnit.MINUTES ))
+            .sign(); 
+    }
+}
