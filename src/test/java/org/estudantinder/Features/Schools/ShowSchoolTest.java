28f93754684f606f98b2b2f3d45819ceffe84e68
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-05-09 23:25:29 -0300

    feat(showSchoolTest): add 404 test

--- src/test/java/org/estudantinder/Features/Schools/ShowSchoolTest.java
+++ src/test/java/org/estudantinder/Features/Schools/ShowSchoolTest.java
@@ -27,4 +27,14 @@ public class ShowSchoolTest {
             .statusCode(200);
         
     }
+
+    @Test
+    public void testNotFoundShowSchoolEndpoint() {
+        given()
+        .pathParam("id", -2)
+        .when().get("/schools/{id}")
+        .then()
+            .statusCode(404);
+        
+    }
 }
