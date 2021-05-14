3e195a3b05cf60617176ecbae2b1e2512af20b40
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-05-14 04:10:24 -0300

    feat(createDislikeTest): add conflict, notFound & badRequest test

--- src/test/java/org/estudantinder/Features/Students/CreateDislikeTest.java
+++ src/test/java/org/estudantinder/Features/Students/CreateDislikeTest.java
@@ -23,6 +23,36 @@ public class CreateDislikeTest {
             .statusCode(201);
     }
 
+    @Test
+    public void testConflictCreateDislikeEndpoint() {
+        given()
+        .pathParam("id", 24)
+        .auth().oauth2(generateValidStudentToken())
+        .when().post("/students/dislikes/{id}")
+        .then()
+            .statusCode(409);
+    }
+
+    @Test
+    public void testNotFoundCreateDislikeEndpoint() {
+        given()
+        .pathParam("id", -24)
+        .auth().oauth2(generateValidStudentToken())
+        .when().post("/students/dislikes/{id}")
+        .then()
+            .statusCode(404);
+    }
+    
+    @Test
+    public void testBadRequestCreateDislikeEndpoint() {
+        given()
+        .pathParam("id", 22)
+        .auth().oauth2(generateValidStudentToken())
+        .when().post("/students/dislikes/{id}")
+        .then()
+            .statusCode(400);
+    }
+
     static String generateValidStudentToken() {
         return Jwt.issuer("https://github.com/AdamAugustinsky")
             .upn("estudantinder@quarkus.io")
