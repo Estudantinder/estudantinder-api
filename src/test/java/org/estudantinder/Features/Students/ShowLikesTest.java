9992a82f63c9e2771b0be0ed93ab1385feabdc82
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-05-14 04:18:15 -0300

    feat(showLikesTest): add notFound test

--- src/test/java/org/estudantinder/Features/Students/ShowLikesTest.java
+++ src/test/java/org/estudantinder/Features/Students/ShowLikesTest.java
@@ -22,11 +22,29 @@ public class ShowLikesTest {
             .statusCode(200);
     }
 
+    @Test
+    public void testNotFoundShowLikesEndpoint() {
+        given()
+        .auth().oauth2(generateNonExistentStudentToken())
+        .when().get("/students/likes")
+        .then()
+            .statusCode(404);
+    }
+
     static String generateValidStudentToken() {
         return Jwt.issuer("https://github.com/AdamAugustinsky")
             .upn("estudantinder@quarkus.io")
             .groups("User")
             .claim("id", 22)
+            .expiresAt(Instant.now().plus(2, ChronoUnit.MINUTES ))
+            .sign();
+    }
+
+    static String generateNonExistentStudentToken() {
+        return Jwt.issuer("https://github.com/AdamAugustinsky")
+            .upn("estudantinder@quarkus.io")
+            .groups("User")
+            .claim("id", -22)
             .expiresAt(Instant.now().plus(2, ChronoUnit.MINUTES ))
             .sign();
     }
