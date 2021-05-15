76c9b9d17985edfb237e08adadd9c4993d7fc7d1
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-05-15 17:33:39 -0300

    feat(showUserFilters): add notFound test

--- src/test/java/org/estudantinder/Features/Users/ShowUserFilters.java
+++ src/test/java/org/estudantinder/Features/Users/ShowUserFilters.java
@@ -17,18 +17,37 @@ public class ShowUserFilters {
     @Test
     public void testEditUserFiltersEndpoint() {
         given()
-        .auth().oauth2(generateValidStudentToken())
+        .auth().oauth2(generateValidUserToken())
         .contentType(ContentType.JSON)
         .when().get("/users/filters")
         .then()
             .statusCode(200);
     }
 
-    static String generateValidStudentToken() {
+    @Test
+    public void testNotFounEditUserFiltersEndpoint() {
+        given()
+        .auth().oauth2(generateNonExistentUserToken())
+        .contentType(ContentType.JSON)
+        .when().get("/users/filters")
+        .then()
+            .statusCode(404);
+    }
+
+    static String generateValidUserToken() {
         return Jwt.issuer("https://github.com/AdamAugustinsky")
             .upn("estudantinder@quarkus.io")
             .groups("User")
             .claim("id", 22)
+            .expiresAt(Instant.now().plus(2, ChronoUnit.MINUTES ))
+            .sign(); 
+    }
+
+    static String generateNonExistentUserToken() {
+        return Jwt.issuer("https://github.com/AdamAugustinsky")
+            .upn("estudantinder@quarkus.io")
+            .groups("User")
+            .claim("id", -22)
             .expiresAt(Instant.now().plus(2, ChronoUnit.MINUTES ))
             .sign(); 
     }
