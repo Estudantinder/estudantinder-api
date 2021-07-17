acd6dd0f85f7b40a3be41edab37af0dbb0fdb13d
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2021-07-17 18:03:01 -0300

    fix: change code to a new project quarkus 2.0 generated with quarkus.io

--- src/main/java/org/estudantinder/features/Students/ShowStudents/Feature.java
+++ src/main/java/org/estudantinder/features/Students/ShowStudents/Feature.java
@@ -10,7 +10,6 @@ import javax.ws.rs.NotFoundException;
 
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.estudantinder.entities.Preferences;
-import org.estudantinder.entities.Subject;
 import org.estudantinder.entities.Users;
 import org.estudantinder.features.Students.common.Student;
 import org.estudantinder.repositories.DislikesRepository;
@@ -30,85 +29,67 @@ public class Feature {
     DislikesRepository dislikesRepository;
 
     void throwExceptionIfUserNotValid(Users authenticatedUser) {
-        if(authenticatedUser == null) {
+        if (authenticatedUser == null) {
             throw new NotFoundException("User id not found");
         }
     }
 
     Stream<Users> removesAuthenticatedUserFromStream(Users authenticatedUser, Stream<Users> allUsers) {
-        return allUsers.filter( user -> 
-            user.getId() != authenticatedUser.getId());
+        return allUsers.filter(user -> user.getId() != authenticatedUser.getId());
     }
 
     Stream<Users> filterUsersByPreferences(Preferences preferences, Stream<Users> allUsers) {
 
-        if(preferences != null ) {
-            if( preferences.getGender() != null ) {
-                if(preferences.getGender().length() != 0) {
-                    allUsers = allUsers.filter( user -> 
-                        user.getGender().equals(preferences.getGender()));
+        if (preferences != null) {
+            if (preferences.getGender() != null) {
+                if (preferences.getGender().length() != 0) {
+                    allUsers = allUsers.filter(user -> user.getGender().equals(preferences.getGender()));
                 }
-            }
-    
-            if(preferences.getShift() != 0) {
-                allUsers = allUsers.filter( user -> 
-                user.getShift() == preferences.getShift());
-            }
-    
-            if(preferences.getSchool_year() != 0 ) {
-                allUsers = allUsers.filter( user -> 
-                    user.getSchool_year() == preferences.getSchool_year() );
-            }
-    
-            if(preferences.getSchool() != null ) {
-                allUsers = allUsers.filter( user -> 
-                    user.getCourse().getSchool() == preferences.getSchool() );
-            }
-            
-            if(preferences.getCourse() != null ) {
-                allUsers = allUsers.filter( user -> 
-                    user.getCourse() == preferences.getCourse() );
             }
 
-            if(preferences.getSubjects() != null ) {
-                if(preferences.getSubjects().size() > 0) {
-                    allUsers = allUsers.filter( user -> {
-                        for(Subject subject : user.getSubjects()) {
-                            if(preferences.getSubjects().contains(subject)) {
-                                return true;
-                            }
-                        }
-                        return false;
-                    });
+            if (preferences.getShift() != 0) {
+                allUsers = allUsers.filter(user -> user.getShift() == preferences.getShift());
+            }
+
+            if (preferences.getSchool_year() != 0) {
+                allUsers = allUsers.filter(user -> user.getSchool_year() == preferences.getSchool_year());
+            }
+
+            if (preferences.getSchool() != null) {
+                allUsers = allUsers.filter(user -> user.getCourse().getSchool() == preferences.getSchool());
+            }
+
+            if (preferences.getCourse() != null) {
+                allUsers = allUsers.filter(user -> user.getCourse() == preferences.getCourse());
+            }
+
+            if (preferences.getSubjects() != null) {
+                if (preferences.getSubjects().size() > 0) {
+                    allUsers = allUsers.filter(user -> preferences.getSubjects()
+                            .stream().anyMatch(subject -> user.getSubjects().contains(subject)));
                 }
             }
-    
+
             return allUsers;
         }
-        
+
         return allUsers;
     }
 
-    Stream<Users> filterUsersAlreadyLiked( Users User, Stream<Users> allUsers) {
-        List<Users> usersAlreadyLiked = likesRepository
-            .stream("sender", User)
-            .map(like -> like.getReceiver())
-            .collect(Collectors.toList());
-        
-        Stream<Users> usersNotLiked = allUsers.filter(s -> 
-            !usersAlreadyLiked.contains(s));
+    Stream<Users> filterUsersAlreadyLiked(Users User, Stream<Users> allUsers) {
+        List<Users> usersAlreadyLiked = likesRepository.stream("sender", User).map(like -> like.getReceiver())
+                .collect(Collectors.toList());
+
+        Stream<Users> usersNotLiked = allUsers.filter(s -> !usersAlreadyLiked.contains(s));
 
         return usersNotLiked;
     }
-    
-    Stream<Users> filterUsersAlreadyDisliked( Users User, Stream<Users> allUsers) {
-        List<Users> usersAlreadyDisliked = dislikesRepository 
-            .stream("sender", User)
-            .map(dislike -> dislike.getReceiver())
-            .collect(Collectors.toList());
-        
-        Stream<Users> usersNotDisliked = allUsers.filter(s -> 
-            !usersAlreadyDisliked.contains(s));
+
+    Stream<Users> filterUsersAlreadyDisliked(Users User, Stream<Users> allUsers) {
+        List<Users> usersAlreadyDisliked = dislikesRepository.stream("sender", User)
+                .map(dislike -> dislike.getReceiver()).collect(Collectors.toList());
+
+        Stream<Users> usersNotDisliked = allUsers.filter(s -> !usersAlreadyDisliked.contains(s));
 
         return usersNotDisliked;
     }
@@ -123,15 +104,14 @@ public class Feature {
 
         return filteredUsers;
     }
-    
+
     List<Student> listFilteredUsers(Users user, Stream<Users> allUsers) {
         Stream<Users> filteredUsers = filteredUsers(user, allUsers);
 
-        Stream<Student> filteredStudents =  filteredUsers.map(s -> Student.mapUserToStudent(s));
+        Stream<Student> filteredStudents = filteredUsers.map(s -> Student.mapUserToStudent(s));
 
         return filteredStudents.collect(Collectors.toList());
     }
-
 
     public List<Student> execute(JsonWebToken jwt) throws Exception {
         long userId = Long.parseLong(jwt.getClaim("id").toString());
@@ -139,7 +119,10 @@ public class Feature {
 
         throwExceptionIfUserNotValid(authenticatedUser);
 
-        Stream<Users> allUsers = usersRepository.streamAll();
-        return listFilteredUsers(authenticatedUser, allUsers);
+        List<Users> allUsers = usersRepository.listAll();
+
+        Stream<Users> allUsersStream = allUsers.stream();
+
+        return listFilteredUsers(authenticatedUser, allUsersStream);
     }
 }
--- src/main/java/org/estudantinder/features/Users/Filters/EditUserFilters/Feature.java
+++ src/main/java/org/estudantinder/features/Users/Filters/EditUserFilters/Feature.java
@@ -9,7 +9,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.BadRequestException;
-import javax.ws.rs.NotFoundException;
 
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.estudantinder.entities.Course;
