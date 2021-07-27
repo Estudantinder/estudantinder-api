9d2cc02701350c7f8022a48d774ceb5ed8f0f254
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2021-07-27 10:19:31 -0300

    feat(likesRepository): add findBySender and findByReceiver

--- src/main/java/org/estudantinder/repositories/LikesRepository.java
+++ src/main/java/org/estudantinder/repositories/LikesRepository.java
@@ -1,12 +1,21 @@
 package org.estudantinder.repositories;
 
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 
 import org.estudantinder.entities.Likes;
+import org.estudantinder.entities.Users;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 @ApplicationScoped
 public class LikesRepository implements PanacheRepository<Likes> {
-    
+    public List<Likes> findBySender(Users user) {
+        return find("sender", user).list();
+    }
+
+    public List<Likes> findByReceiver(Users user) {
+        return find("receiver", user).list();
+    }
 }
