a567c41928f1be21db1b90e978371182d9cbf76d
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2021-08-02 11:07:41 -0300

    feat: add Admin Role validation to Subject features

--- src/main/java/org/estudantinder/features/Subjects/UpdateSubject/Resource.java
+++ src/main/java/org/estudantinder/features/Subjects/UpdateSubject/Resource.java
@@ -1,5 +1,6 @@
 package org.estudantinder.features.Subjects.UpdateSubject;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
@@ -11,13 +12,17 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("subjects")
 @Tag(name = "Subjects")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@SecurityScheme(securitySchemeName = "jwt", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "jwt")
 public class Resource {
     
     @Inject
@@ -26,6 +31,8 @@ public class Resource {
     @PUT
     @Path("{id}")
     @Transactional
+    @RolesAllowed("Admin")
+    @SecurityRequirement(name = "jwt")
     @APIResponse(responseCode = "200", description = "Successfully Updated")
     @APIResponse(responseCode = "404", description = "Subject ID Not Found")
     @APIResponse(responseCode = "500", description = "Unexpected Error")
