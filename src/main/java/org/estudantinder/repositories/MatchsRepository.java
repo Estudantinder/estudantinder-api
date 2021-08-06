a52786e4e1bc92a821bb8e775f4c3da4d5c2bbfb
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2021-08-06 12:39:13 -0300

    feat(matchsRepository): add findByLike

--- src/main/java/org/estudantinder/repositories/MatchsRepository.java
+++ src/main/java/org/estudantinder/repositories/MatchsRepository.java
@@ -2,11 +2,18 @@ package org.estudantinder.repositories;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.estudantinder.entities.Like;
 import org.estudantinder.entities.Match;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 @ApplicationScoped
 public class MatchsRepository implements PanacheRepository<Match> {
-    
+    public Match findByLike(Like like) {
+        return find("like", like).firstResult();
+    }
+
+    public Match findByMutualLike(Like like) {
+        return find("mutualLike", like).firstResult();
+    }
 }
