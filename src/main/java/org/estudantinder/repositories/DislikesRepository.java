d9ba8e52f83dc0088e66a8c768ff2c85651319d1
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2021-08-06 12:39:29 -0300

    feat(dislikesRepository): add findByUser

--- src/main/java/org/estudantinder/repositories/DislikesRepository.java
+++ src/main/java/org/estudantinder/repositories/DislikesRepository.java
@@ -1,12 +1,21 @@
 package org.estudantinder.repositories;
 
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 
 import org.estudantinder.entities.Dislike;
+import org.estudantinder.entities.User;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 @ApplicationScoped
 public class DislikesRepository implements PanacheRepository<Dislike> {
-    
+    public List<Dislike> findBySender(User user) {
+        return find("sender", user).list();
+    }
+
+    public List<Dislike> findByReceiver(User user) {
+        return find("receiver", user).list();
+    }
 }
