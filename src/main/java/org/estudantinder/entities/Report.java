0a2b219b107bfe6dcbbf8e2a113b5eb048573179
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2021-08-07 11:36:58 -0300

    feat(Report): add reportDate to entity

--- src/main/java/org/estudantinder/entities/Report.java
+++ src/main/java/org/estudantinder/entities/Report.java
@@ -1,5 +1,7 @@
 package org.estudantinder.entities;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -15,9 +17,15 @@ public class Report {
     private String title;
 
     private String description;
+
+    private LocalDate reportDate;
     
     @ManyToOne
     private User reportedUser;
+
+    public Long getId() {
+        return id;
+    }
 
     public User getReportedUser() {
         return reportedUser;
@@ -25,6 +33,14 @@ public class Report {
 
     public String getDescription() {
         return description;
+    }
+
+    public LocalDate getReportDate() {
+        return reportDate;
+    }
+
+    public void setReportDate(LocalDate report_date) {
+        this.reportDate = report_date;
     }
 
     public void setDescription(String description) {
