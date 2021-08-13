7cf01a472168570ed272e177b890b0259b10f192
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2021-08-13 10:46:32 -0300

    feat(ReportsRepository): add findByUser

--- src/main/java/org/estudantinder/repositories/ReportsRepository.java
+++ src/main/java/org/estudantinder/repositories/ReportsRepository.java
@@ -1,12 +1,19 @@
 package org.estudantinder.repositories;
 
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 
 import org.estudantinder.entities.Report;
+import org.estudantinder.entities.User;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 @ApplicationScoped
 public class ReportsRepository implements PanacheRepository<Report> {
     
+    // find report by user
+    public List<Report> findByUser(User user) {
+        return find("reportedUser", user).list();
+    }   
 }
