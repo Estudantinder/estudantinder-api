1a7dc0576d30df996bef9989250d2f21fd42a41b
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2021-08-13 10:49:40 -0300

    test(DeleteReport)

---
+++ src/test/java/org/estudantinder/Features/Reports/DeleteReportTest.java
@@ -0,0 +1,34 @@
+package org.estudantinder.Features.Reports;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.jwt.build.Jwt;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Set;
+
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+public class DeleteReportTest {
+
+    @Test
+    public void testBanReportedUserEndpoint() {
+
+        given()
+            .pathParam("reportId", 36)
+            .auth().oauth2(generateValidUserToken())
+            .when().delete("/report/{reportId}")
+            .then()
+                .statusCode(200);
+    }
+
+    static String generateValidUserToken() {
+        return Jwt.issuer("https://github.com/AdamAugustinsky").upn("estudantinder@quarkus.io")
+                .groups(Set.of("Admin", "User")).claim("id", 22).expiresAt(Instant.now().plus(2, ChronoUnit.MINUTES))
+                .sign();
+    }
+}
