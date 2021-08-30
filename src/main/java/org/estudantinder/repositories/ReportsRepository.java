b814167805f60dd8012de7ed3b242c39de4bb73a
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2021-08-29 21:53:16 -0300

    feat(ReportsRepository): add new find methods

--- src/main/java/org/estudantinder/repositories/ReportsRepository.java
+++ src/main/java/org/estudantinder/repositories/ReportsRepository.java
@@ -8,6 +8,7 @@ import org.estudantinder.entities.Report;
 import org.estudantinder.entities.User;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
 
 @ApplicationScoped
 public class ReportsRepository implements PanacheRepository<Report> {
@@ -16,4 +17,17 @@ public class ReportsRepository implements PanacheRepository<Report> {
     public List<Report> findByUser(User user) {
         return find("reportedUser", user).list();
     }   
+
+    // find report by type
+    public List<Report> findByType(String type) {
+        return find("type", type).list();
+    }   
+
+    // find report by user and type
+    public List<Report> findByUserAndType(User user, String type) {
+        return find("reportedUser = :reportedUser and type = :type", 
+            Parameters.with("reportedUser", user)
+            .and("type", type)).list();
+    }
+
 }
