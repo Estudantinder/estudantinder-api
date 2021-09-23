5af4444fe91ded19c3de5e272adf60b8cafb6706
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-09-23 13:52:41 -0300

    add: NotEmpty validation to CreateCourseDTO

--- src/main/java/org/estudantinder/domain/dto/CreateCourseDTO.java
+++ src/main/java/org/estudantinder/domain/dto/CreateCourseDTO.java
@@ -1,5 +1,8 @@
 package org.estudantinder.domain.dto;
 
+import javax.validation.constraints.NotEmpty;
+
 public class CreateCourseDTO {
+    @NotEmpty(message = "Nome do curso não pode ser vazio")
     public String name;
 }
