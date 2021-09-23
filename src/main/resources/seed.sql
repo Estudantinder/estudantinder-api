6540cd111ba6f3c7b1ca58ed32262b50e7b9bccb
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-09-23 19:28:44 -0300

    add: seed.sql and load it at start

---
+++ src/main/resources/seed.sql
@@ -0,0 +1,9 @@
+insert into "school" ("id", "address", "name") values ('b68fa2c4-0463-4783-8ac0-c97edd0d34f4', 'School 1 Adress', 'School 1');
+insert into "school" ("id", "address", "name") values ('b68fa2c4-0463-4783-8ac0-c97edd0d32f3', 'School 2 Adress', 'School 2');
+insert into "school" ("id", "address", "name") values ('b68fa2c4-0463-4783-8ac0-c97edd0d32f2', 'School 3 Adress', 'School 3');
+insert into "school" ("id", "address", "name") values ('b68fa2c4-0463-4783-8ac0-c97edd0d32f1', 'School 4 Adress', 'School 3');
+
+insert into "course" ("id", "name", "school_id") values ('37a77482-61ea-45e5-935e-2b487a1c0299', 'course 1', 'b68fa2c4-0463-4783-8ac0-c97edd0d34f4');
+insert into "course" ("id", "name", "school_id") values ('37a77482-61ea-45e5-935e-2b487a1c0298', 'course 2', 'b68fa2c4-0463-4783-8ac0-c97edd0d32f3');
+insert into "course" ("id", "name", "school_id") values ('37a77482-61ea-45e5-935e-2b487a1c0297', 'course 3', 'b68fa2c4-0463-4783-8ac0-c97edd0d32f2');
+insert into "course" ("id", "name", "school_id") values ('37a77482-61ea-45e5-935e-2b487a1c0296', 'course 4', 'b68fa2c4-0463-4783-8ac0-c97edd0d32f1');
