46e57441ab9ac2346e9b6bb0b3e15069be9a1ce8
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-09-23 11:55:20 -0300

    add(CreateSchoolResource): APIResponse annotations

--- src/main/java/org/estudantinder/presentation/CreateSchoolResource.java
+++ src/main/java/org/estudantinder/presentation/CreateSchoolResource.java
@@ -10,10 +10,15 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.estudantinder.domain.dto.CreateSchoolDTO;
+import org.estudantinder.domain.models.School;
 import org.estudantinder.main.CreateSchoolService;
+import org.estudantinder.shared.exception.mapper.ErrorResponse;
 
 @Path("/schools")
 @Tag(name = "Schools")
@@ -27,7 +32,24 @@ public class CreateSchoolResource {
     @POST
     @Transactional
     @Operation(summary = "Create a new School")
+    @APIResponse(
+        responseCode = "201",
+        description = "School created",
+        content = @Content(
+            mediaType = "application/json", 
+            schema = @Schema(implementation = School.class)
+        ))
+    @APIResponse(
+        responseCode = "409",
+        description = "School already exists",
+        content = @Content(
+            mediaType = "application/json", 
+            schema = @Schema(implementation = ErrorResponse.class)
+        ))
     public Response handle(@RequestBody CreateSchoolDTO createSchoolDTO) {
-        return Response.status(201).entity(createSchoolService.handle(createSchoolDTO)).build();
+        return Response
+            .status(201)
+            .entity(createSchoolService.handle(createSchoolDTO))
+            .build();
     }
 }
