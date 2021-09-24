2c0e2a3d7ce30ec31bfaf6f68ff5d8fb23db2240
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-09-23 22:12:44 -0300

    add: ListSchoolsResource to presentation

---
+++ src/main/java/org/estudantinder/presentation/ListSchoolsResource.java
@@ -0,0 +1,44 @@
+package org.estudantinder.presentation;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.estudantinder.domain.models.School;
+import org.estudantinder.main.ListSchoolsService;
+
+@Path("schools")
+@Tag(name = "School")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ListSchoolsResource {
+    
+    @Inject
+    ListSchoolsService listSchoolsService;
+
+    @GET
+    @Operation(summary = "List all schools")
+    @APIResponse(
+        responseCode = "200",
+        description = "OK",
+        content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = School[].class)
+        )
+    )
+    public Response handle() {
+        return Response
+            .status(200)
+            .entity(listSchoolsService.handle())
+            .build();
+    }
+}
