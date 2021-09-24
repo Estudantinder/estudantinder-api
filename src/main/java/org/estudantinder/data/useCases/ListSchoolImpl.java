fb77524ff853cb1d50e341c6b0a60f1fb2f45677
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-09-23 22:14:35 -0300

    add: ListSchoolImpl to data layer

---
+++ src/main/java/org/estudantinder/data/useCases/ListSchoolImpl.java
@@ -0,0 +1,29 @@
+package org.estudantinder.data.useCases;
+
+import java.util.UUID;
+
+import javax.ws.rs.WebApplicationException;
+
+import org.estudantinder.data.interfaces.SchoolRepository;
+import org.estudantinder.domain.models.School;
+import org.estudantinder.domain.useCases.ListSchool;
+
+public class ListSchoolImpl implements ListSchool {
+
+    private SchoolRepository schoolRepository;
+
+    public ListSchoolImpl(SchoolRepository schoolRepository) {
+        this.schoolRepository = schoolRepository;
+    }
+
+    @Override
+    public School findById(UUID schoolId) {
+        School school = this.schoolRepository.findById(schoolId);
+
+        if(school == null)
+            throw new WebApplicationException("Escola não encontrada");
+
+        return school;
+    }
+
+}
