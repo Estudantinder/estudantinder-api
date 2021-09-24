f62132c877c17305e48f1686d92ab91c93cd48a4
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-09-23 22:06:34 -0300

    add(SchoolRepository): listAll schools

--- src/main/java/org/estudantinder/data/interfaces/SchoolRepository.java
+++ src/main/java/org/estudantinder/data/interfaces/SchoolRepository.java
@@ -1,9 +1,13 @@
 package org.estudantinder.data.interfaces;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.estudantinder.domain.dto.CreateSchoolDTO;
 import org.estudantinder.domain.models.School;
 
 public interface SchoolRepository {
     School findByName(String name);
     School create(CreateSchoolDTO school);
+    List<School> listAll();
 }
