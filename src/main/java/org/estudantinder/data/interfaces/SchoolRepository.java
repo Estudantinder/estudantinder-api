e40b1bafac2859f3716264d9f48fc6c265b9006a
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-09-24 00:18:08 -0300

    delete: all CreateSchool related files/functions

--- src/main/java/org/estudantinder/data/interfaces/SchoolRepository.java
+++ src/main/java/org/estudantinder/data/interfaces/SchoolRepository.java
@@ -3,12 +3,9 @@ package org.estudantinder.data.interfaces;
 import java.util.List;
 import java.util.UUID;
 
-import org.estudantinder.domain.dto.CreateSchoolDTO;
 import org.estudantinder.domain.models.School;
 
 public interface SchoolRepository {
     School findById(UUID schoolId);
-    School findByName(String name);
-    School create(CreateSchoolDTO school);
     List<School> listAll();
 }
--- src/main/java/org/estudantinder/domain/useCases/CreateSchool.java
+++
@@ -1,8 +0,0 @@
-package org.estudantinder.domain.useCases;
-
-import org.estudantinder.domain.dto.CreateSchoolDTO;
-import org.estudantinder.domain.models.School;
-
-public interface CreateSchool {
-    School create(CreateSchoolDTO school);
-}
