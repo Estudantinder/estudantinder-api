29771499ea5422bfc7edc6a4b7784655a670f8a9
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-10-21 14:23:39 -0300

    refactor: remove unused statistics routes

--- src/main/java/org/estudantinder/features/Statistics/Dashboard/DTO/UsersPerCourseDTO.java
+++
@@ -1,7 +0,0 @@
-package org.estudantinder.features.Statistics.Dashboard.DTO;
-
-public class UsersPerCourseDTO {
-    public String course_name;
-    public Long number_of_users;
-    public Double percent;
-}
--- src/main/java/org/estudantinder/features/Statistics/Dashboard/DTO/UsersPerSchoolDTO.java
+++
@@ -1,7 +0,0 @@
-package org.estudantinder.features.Statistics.Dashboard.DTO;
-
-public class UsersPerSchoolDTO {
-    public String school_name;
-    public Long number_of_users;
-    public Double percent;
-}
